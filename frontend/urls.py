5cbd8e973862df06343078348c5adf0c5626e91e
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2025-10-11 16:03:33 +0330

    u

--- frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'frontend'
-
-urlpatterns = [
-    path('', views.main_view, name='main'),
-    path('friend/<str:username>/', views.friend_page, name='friend_page'),
-]
--- streams/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/presence/$', consumers.PresenceConsumer.as_asgi()),
-    re_path(r'ws/stream/(?P<username>\w+)/$', consumers.StreamConsumer.as_asgi()),
-]
